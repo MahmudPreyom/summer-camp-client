c32d9f76ae9ed2ae58c36d238ffaa1f683697fc1
MahmudPreyom <preyom5076@gmail.com>
2023-06-07 20:39:07 +0600

    login form add

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-zinc-300">
+        <div className="navbar fixed z-10 bg-opacity-30 bg-black text-white max-w-screen-xl">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link>Home</Link></li>
+                        <li><Link to="/">Home</Link></li>
                         <li><Link>Instructors</Link> </li>
                         <li><Link>Classes</Link></li>
                         <li><Link>Dashboard</Link></li>
@@ -21,14 +21,14 @@ const Navbar = () => {
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><Link>Home</Link></li>
+                    <li><Link to="/">Home</Link></li>
                     <li><Link>Instructors</Link></li>
                     <li><Link>Classes</Link></li>
                     <li><Link>Dashboard</Link></li>
                 </ul>
             </div>
             <div className="navbar-end">
-                <Link className="btn btn-sm">Login</Link>
+                <Link to="login" className="btn btn-sm">Login</Link>
                 <Link className="btn btn-sm">Log Out</Link>
             </div>
         </div>
