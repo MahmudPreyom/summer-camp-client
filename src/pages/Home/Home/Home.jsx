42cabd1390540048ee93027c8f7ae7d13ed2914e
MahmudPreyom <preyom5076@gmail.com>
2023-06-07 12:47:03 +0600

    first commit basic route set up

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-red-600 text-3xl">This is home</h2>
+        </div>
+    );
+};
+
+export default Home;
